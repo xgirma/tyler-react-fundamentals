cdf777ab914c82131a07e8179a2147b4084dd651
Girma Nigusse <xgirma@gmail.com>
2017-11-02 01:05:05 -0700

    stateless functional component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,33 @@
 const React = require('react');
+const PropTypes = require('prop-types');
+
+class SelectLanguage extends React.Component {
+    render () {
+        const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+        return (
+            <div>
+                <ul className='languages'>
+                    {languages.map((lang) => {
+                        return (
+                            <li
+                                style={lang === this.props.selectedLanguage? { color: '#d0021b'}: null}
+                                key={lang}
+                                onClick={this.props.onSelect.bind(null, lang)}>
+                                {lang}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
+
+SelectLanguage.propTypes = {
+    selectedLanguage: PropTypes.string.isRequired,
+    onSelect: PropTypes.func.isRequired,
+};
 
 class Popular extends React.Component {
     constructor(props) {
@@ -19,22 +48,12 @@ class Popular extends React.Component {
     }
 
     render() {
-        const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 
         return (
             <div>
-                <ul className='languages'>
-                {languages.map((lang) => {
-                    return (
-                        <li
-                            style={lang === this.state.selectedLanguage? { color: '#d0021b'}: null}
-                            key={lang}
-                            onClick={this.updateLanguage.bind(null, lang)}>
-                            {lang}
-                        </li>
-                    )
-                })}
-                </ul>
+                <SelectLanguage selectedLanguage={this.state.selectedLanguage}
+                                onSelect={this.updateLanguage}
+                />
             </div>
         );
     }
